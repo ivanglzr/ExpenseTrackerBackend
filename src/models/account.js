beab446694119d6432d9e23a1b6e1978bd79c12c
Ivan <ivanglzr01@gmail.com>
2024-08-31 22:34:40 +0200

    account model created

---
+++ src/models/account.js
@@ -0,0 +1,47 @@
+import { Schema } from "mongoose";
+
+const AccountSchema = new Schema({
+  accountName: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  transactions: [
+    {
+      type: {
+        type: String,
+        enum: {
+          values: ["income", "expense"],
+          message:
+            '{VALUE} is not a valid transaction type. Valid types are "income" and "expense".',
+        },
+        required: [true, "Transaction type is required"],
+      },
+      amount: {
+        type: Number,
+        required: [true, "Transaction amount is required"],
+      },
+      currency: {
+        type: String,
+        required: [true, "Currency is required"],
+        default: "USD",
+      },
+      category: {
+        type: String,
+        required: [true, "Category is required"],
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+});
+
+export default AccountSchema;
