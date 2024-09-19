6f0d5bac93fdba2a6be4a1ecc9660383c8681918
Ivan <ivanglzr01@gmail.com>
2024-09-19 09:31:15 +0200

    add patch user to router

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { postUser, loginUser } from "../controllers/user.js";
+import { postUser, loginUser, logoutUser } from "../controllers/user.js";
 
 const userRouter = Router();
 
 userRouter.post("/register", postUser);
 userRouter.post("/login", loginUser);
+userRouter.post("/logout", logoutUser);
 
 export default userRouter;
