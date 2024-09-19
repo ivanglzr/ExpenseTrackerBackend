a5f118a513b37164de7e083232f3f02dd6831ed7
Ivan <ivanglzr01@gmail.com>
2024-09-19 09:54:11 +0200

    patch user implemented

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
 
-import { postUser, loginUser, logoutUser } from "../controllers/user.js";
+import {
+  postUser,
+  loginUser,
+  logoutUser,
+  patchUser,
+} from "../controllers/user.js";
+
+import validateUser from "../middlewares/validateUser.js";
 
 const userRouter = Router();
 
-userRouter.post("/register", postUser);
+userRouter.post("/", postUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/logout", logoutUser);
+
+userRouter.use(validateUser);
+
+userRouter.patch("/", patchUser);
 
 export default userRouter;
