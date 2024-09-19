741d6735f874ba5e387b97a93190eeb0dfb37a1b
Ivan <ivanglzr01@gmail.com>
2024-09-19 09:53:51 +0200

    validateUser middleware

---
+++ src/middlewares/validateUser.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+import { statusMessages } from "../config.js";
+
+export default function validateUser(req, res, next) {
+  const token = req.cookies.access_token;
+
+  if (!token)
+    return res.status(401).json({
+      status: statusMessages.error,
+      message: "You must login",
+    });
+
+  try {
+    const { id } = jwt.verify(token, process.env.JWT_KEY);
+
+    if (id === undefined || id === null || id.length !== 24) {
+      return res.status(400).json({
+        status: statusMessages.error,
+        message: "Id isn't valid",
+      });
+    }
+
+    req.session = { id: id };
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      status: statusMessages.error,
+      message: "Unauthorized",
+    });
+  }
+}
