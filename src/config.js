84bf970d90ff4e1f8f602e355b4bca849dd4d7f6
Ivan <ivanglzr01@gmail.com>
2024-10-07 18:44:25 +0200

    add new constants to avoid the use of strings

--- src/config.js
+++ src/config.js
@@ -1,3 +1,34 @@
+export const cookieName = "access_token";
+
+export const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "lax",
+  maxAge: 60 * 60 * 1000,
+};
+
+export const userObject = {
+  fullname: "fullname",
+  email: "email",
+  password: "password",
+  accounts: "accounts",
+};
+
+export const accountObject = {
+  accountName: "accountName",
+  balance: "balance",
+  transactions: "transactions",
+};
+
+export const transactionObject = {
+  type: "type",
+  amount: "amount",
+  currency: "currency",
+  category: "category",
+  date: "date",
+  description: "description",
+};
+
 export const statusMessages = {
   success: "success",
   error: "error",
