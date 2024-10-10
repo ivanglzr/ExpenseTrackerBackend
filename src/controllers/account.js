59c78e4c6beb77d4a3a21ee739e9265f15b1f776
Ivan <ivanglzr01@gmail.com>
2024-10-10 19:19:46 +0200

    controller for getting an account by id

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,12 +1,12 @@
 import User from "../models/user.js";
 
-import { statusMessages } from "../config.js";
+import { userObject, statusMessages, accountIdName } from "../config.js";
 
 export async function getAccounts(req, res) {
   const { id } = req.session;
 
   try {
-    const { accounts } = await User.findById(id, "accounts");
+    const { accounts } = await User.findById(id, userObject.accounts);
 
     const message =
       accounts.length === 0
@@ -22,6 +22,36 @@ export async function getAccounts(req, res) {
     return res.status(500).json({
       status: statusMessages.error,
       message: "An error occurred while finding the accounts",
+    });
+  }
+}
+
+export async function getAccount(req, res) {
+  const { id } = req.session;
+  const accountId = req.params[accountIdName];
+
+  try {
+    const { accounts } = await User.findById(id, userObject.accounts);
+
+    const account = accounts.find(
+      (account) => account._id.toString() === accountId
+    );
+
+    if (!account)
+      return res.status(404).json({
+        status: statusMessages.error,
+        message: "Account not found",
+      });
+
+    return res.json({
+      status: statusMessages.success,
+      message: "Account found",
+      account,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: statusMessages.error,
+      message: "An error ocurred while finding the account",
     });
   }
 }
