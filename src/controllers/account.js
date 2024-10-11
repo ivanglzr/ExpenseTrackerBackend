e7ca7956a8ffbb20b750454bd1d80c9d108e08e8
Ivan <ivanglzr01@gmail.com>
2024-10-11 15:41:21 +0200

    controller for modifying the account

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,6 +1,6 @@
 import User from "../models/user.js";
 
-import { validateAccount } from "../schemas/account.js";
+import { validateAccount, validatePartialAccount } from "../schemas/account.js";
 
 import {
   userObject,
@@ -69,13 +69,13 @@ export async function postAccount(req, res) {
   if (error)
     return res.status(422).json({
       status: statusMessages.error,
-      message: "Account isn't valid",
+      message: error.errors[0].message,
     });
 
   const { id } = req.session;
 
   try {
-    const user = await User.findById(id);
+    const user = await User.findById(id, userObject.accounts);
 
     if (!user)
       return res.status(404).json({
@@ -106,6 +106,58 @@ export async function postAccount(req, res) {
     return res.status(500).json({
       status: statusMessages.error,
       message: "An error occurred while posting the account",
+    });
+  }
+}
+
+export async function patchAccount(req, res) {
+  const { data, error } = validatePartialAccount(req.body);
+
+  if (error)
+    return res.status(422).json({
+      status: statusMessages.error,
+      message: error.errors[0].message,
+    });
+
+  const { id } = req.session;
+  const accountId = req.params[accountIdName];
+
+  try {
+    const user = await User.findById(id, userObject.accounts);
+
+    if (!user)
+      return res.status(404).json({
+        status: statusMessages.error,
+        message: "User doesn't exists",
+      });
+
+    const accountIndex = user[userObject.accounts].findIndex(
+      (account) => account._id.toString() === accountId
+    );
+
+    if (accountIndex === -1)
+      return res.status(404).json({
+        status: statusMessages.error,
+        message: "Account not found",
+      });
+
+    const newAccount = {
+      ...user[userObject.accounts][accountIndex],
+      ...data,
+    };
+
+    user[userObject.accounts][accountIndex] = newAccount;
+
+    await user.save();
+
+    return res.json({
+      status: statusMessages.success,
+      message: "Account edited correctly",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: statusMessages.error,
+      message: "An error occurred while",
     });
   }
 }
